4d0cd9e3cffcda7dbd46f69ab7431c018c2fbcf4
ebazhanov <evg.bazhanov@yahoo.com>
2022-10-01 07:28:38 +0200

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+      <div className="slider">
+        <div className="slider">
+          <div className="slider__slider__content">
+            <div className="slider__icon">
+              <span className="material-symbols-outlined">star</span>
+            </div>
+            <div className="slider__title">Slide 1</div>
+            <div className="slider__description">Some description</div>
+          </div>
+        </div>
+        <div className="slider">
+          <div className="slider__slider__content">
+            <div className="slider__icon"></div>
+            <div className="slider__title">Slide 2</div>
+            <div className="slider__description">Some description</div>
+          </div>
+        </div>
+        <div className="slider">
+          <div className="slider__slider__content">
+            <div className="slider__icon"></div>
+            <div className="slider__title">Slide 3</div>
+            <div className="slider__description">Some description</div>
+          </div>
+        </div>
+        <div className="slider">
+          <div className="slider__slider__content">
+            <div className="slider__icon"></div>
+            <div className="slider__title">Slide 4</div>
+            <div className="slider__description">Some description</div>
+          </div>
+        </div>
+        <div className="slider">
+          <div className="slider__slider__content">
+            <div className="slider__icon"></div>
+            <div className="slider__title">Slide 5</div>
+            <div className="slider__description">Some description</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
