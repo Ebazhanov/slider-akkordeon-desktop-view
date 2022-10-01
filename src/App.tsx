540f6024c20642eb13db66c49a2e77c92a3542d2
ebazhanov <evg.bazhanov@yahoo.com>
2022-10-01 07:43:17 +0200

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
-    <div className="App" >
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
+    <div className="App">
       <div className="slider">
         <div className="slider">
           <div className="slider__slider__content">
@@ -20,28 +16,40 @@ function App() {
         </div>
         <div className="slider">
           <div className="slider__slider__content">
-            <div className="slider__icon"></div>
+            <div className="slider__icon">
+              <span className="material-symbols-outlined">
+                shopping_cart_checkout
+              </span>
+            </div>
             <div className="slider__title">Slide 2</div>
             <div className="slider__description">Some description</div>
           </div>
         </div>
         <div className="slider">
           <div className="slider__slider__content">
-            <div className="slider__icon"></div>
+            <div className="slider__icon">
+              <span className="material-symbols-outlined">sunny</span>
+            </div>
             <div className="slider__title">Slide 3</div>
             <div className="slider__description">Some description</div>
           </div>
         </div>
         <div className="slider">
           <div className="slider__slider__content">
-            <div className="slider__icon"></div>
+            <div className="slider__icon">
+              <span className="material-symbols-outlined">
+                tips_and_updates
+              </span>
+            </div>
             <div className="slider__title">Slide 4</div>
             <div className="slider__description">Some description</div>
           </div>
         </div>
         <div className="slider">
           <div className="slider__slider__content">
-            <div className="slider__icon"></div>
+            <div className="slider__icon">
+              <span className="material-symbols-outlined">category</span>
+            </div>
             <div className="slider__title">Slide 5</div>
             <div className="slider__description">Some description</div>
           </div>
