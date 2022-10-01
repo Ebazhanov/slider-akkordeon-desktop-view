a58ca3cec1bb7a48f608db4f7499702740bc87f1
ebazhanov <evg.bazhanov@yahoo.com>
2022-10-01 16:27:05 +0200

    stated with styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="slider">
-        <div className="slider">
-          <div className="slider__slider__content">
+        <div className="slide">
+          <div className="slider__content">
             <div className="slider__icon">
               <span className="material-symbols-outlined">star</span>
             </div>
@@ -14,46 +14,46 @@ function App() {
             <div className="slider__description">Some description</div>
           </div>
         </div>
-        <div className="slider">
-          <div className="slider__slider__content">
-            <div className="slider__icon">
+        {/*        <div className="slide">
+          <div className="slide__content">
+            <div className="slide__icon">
               <span className="material-symbols-outlined">
                 shopping_cart_checkout
               </span>
             </div>
-            <div className="slider__title">Slide 2</div>
-            <div className="slider__description">Some description</div>
+            <div className="slide__title">Slide 2</div>
+            <div className="slide__description">Some description</div>
           </div>
         </div>
-        <div className="slider">
-          <div className="slider__slider__content">
-            <div className="slider__icon">
+        <div className="slide">
+          <div className="slide__content">
+            <div className="slide__icon">
               <span className="material-symbols-outlined">sunny</span>
             </div>
             <div className="slider__title">Slide 3</div>
             <div className="slider__description">Some description</div>
           </div>
         </div>
-        <div className="slider">
-          <div className="slider__slider__content">
-            <div className="slider__icon">
+        <div className="slide">
+          <div className="slide__content">
+            <div className="slide__icon">
               <span className="material-symbols-outlined">
                 tips_and_updates
               </span>
             </div>
-            <div className="slider__title">Slide 4</div>
-            <div className="slider__description">Some description</div>
+            <div className="slide__title">Slide 4</div>
+            <div className="slide__description">Some description</div>
           </div>
         </div>
-        <div className="slider">
-          <div className="slider__slider__content">
-            <div className="slider__icon">
+        <div className="slide">
+          <div className="slide__content">
+            <div className="slide__icon">
               <span className="material-symbols-outlined">category</span>
             </div>
-            <div className="slider__title">Slide 5</div>
-            <div className="slider__description">Some description</div>
+            <div className="slide__title">Slide 5</div>
+            <div className="slide__description">Some description</div>
           </div>
-        </div>
+        </div>*/}
       </div>
     </div>
   );
